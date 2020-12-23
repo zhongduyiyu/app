af3fd6b268a5026d7e1125085544a5b1489f37a4
Your Name <401989942@qq.com>
2020-12-23 09:50:08 +0800

    修改一些bug

---
+++ src/layouts/BlankLayout.vue
@@ -0,0 +1,24 @@
+<!--
+ * @Description: 
+ * @Version: 0.01
+ * @Autor: MoXu
+ * @Date: 2020-12-21 09:40:24
+ * @LastEditors: MoXu
+ * @LastEditTime: 2020-12-23 09:45:58
+-->
+<template>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'BlankLayout'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 /*
-name:App
-version:0.01
-author:MoXU
-*/ 
+ * @Description: 
+ * @Version: 0.01
+ * @Autor: MoXu
+ * @Date: 2020-12-16 09:20:56
+ * @LastEditors: MoXu
+ * @LastEditTime: 2020-12-23 09:49:36
+ */
 
 import Vue from 'vue'
 import App from './App.vue'
@@ -11,15 +14,14 @@ import store from './store'
 //全局引入antCss文件 --上线前需按需引入
 import 'ant-design-vue/dist/antd.css'
 //全局引入antd核心文件
-import Antd from 'ant-design-vue';
-//开启dev模式
+import Antd from "ant-design-vue"
 
+//引入quill富文本编辑器
 import QuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.bubble.css'
 import 'quill/dist/quill.snow.css'
 //注入Vue-quill-editor
-
 Vue.use(QuillEditor)
 
 //滚动条
@@ -27,10 +29,11 @@ import vuescroll from 'vuescroll';
 Vue.use(vuescroll, {
   ops: {}, // 在这里设置全局默认配置
 });
-
+//开启dev模式
 Vue.config.productionTip = true
 //注入antd
 Vue.use(Antd)
+
 new Vue({
   router,
   store,
